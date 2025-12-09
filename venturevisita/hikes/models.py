215dd872535159ae2affff7fc4cb3a5f25ba1f75
wochuna <wochunayvonne@gmail.com>
2025-12-09 22:45:48 +0300

    Set the default price as zero

--- venturevisita/hikes/models.py
+++ venturevisita/hikes/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from users.models import User
+from django.utils import timezone
+
 
 class Hike(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    price = models.DecimalField(max_digits=8, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
     location = models.CharField(max_length=200)
     provider = models.ForeignKey(User, on_delete=models.CASCADE, related_name='hikes')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+    
--- venturevisita/users/models.py
+++ venturevisita/users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class User(models.Model):
     USER_ROLES = [
