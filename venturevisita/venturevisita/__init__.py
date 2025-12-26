13e108bf8284d2066ebd4a9c847ac42f87274296
wochuna <wochunayvonne@gmail.com>
2025-12-26 22:00:39 +0300

    Configure database and ignore settings file

---
+++ venturevisita/venturevisita/__init__.py
@@ -0,0 +1,4 @@
+# Use PyMySQL as a drop-in replacement for MySQLdb
+import pymysql
+
+pymysql.install_as_MySQLdb()
--- venturevisita/venturevisita/settings.py
+++ venturevisita/venturevisita/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'venturevisita.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'venturevisita',
+        'USER': 'root',
+        'PASSWORD': 'ChuiCube42KGBlue@',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
